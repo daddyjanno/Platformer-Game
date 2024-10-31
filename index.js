496095449e95b9d0a54c046950027fe9ebb1c1b8
daddyjanno <jn.drugmand@gmail.com>
2024-10-31 12:09:57 +0100

    introducing object oriented method

--- index.js
+++ index.js
@@ -5,20 +5,30 @@ console.log(context);
 canvas.width = 1024;
 canvas.height = 576;
 
-let y = 100;
-let y2 = 100;
+class Player {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+  }
+  draw() {
+    context.fillStyle = "red";
+    context.fillRect(this.position.x, this.position.y, 100, 100);
+  }
+
+  update() {
+    this.position.y += 1;
+  }
+}
+
+const player = new Player();
 
 function animate() {
   window.requestAnimationFrame(animate);
-
   context.fillStyle = "white";
   context.fillRect(0, 0, canvas.width, canvas.height);
-
-  context.fillStyle = "red";
-  context.fillRect(200, y, 100, 100);
-  y += 1;
-  context.fillStyle = "red";
-  context.fillRect(400, y2, 100, 100);
-  y2 += 1;
+  player.draw();
+  player.update();
 }
 animate();
