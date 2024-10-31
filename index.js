b86e3a06c79e07c1a9ebcb740007645107ffbb03
daddyjanno <jn.drugmand@gmail.com>
2024-10-31 13:03:37 +0100

    gravity

--- index.js
+++ index.js
@@ -5,30 +5,43 @@ console.log(context);
 canvas.width = 1024;
 canvas.height = 576;
 
+const gravity = 0.5;
+
 class Player {
-  constructor() {
-    this.position = {
+  constructor(position) {
+    this.position = position;
+    this.velocity = {
       x: 0,
-      y: 0,
+      y: 1,
     };
+    this.height = 100;
   }
   draw() {
     context.fillStyle = "red";
-    context.fillRect(this.position.x, this.position.y, 100, 100);
+    context.fillRect(this.position.x, this.position.y, 100, this.height);
   }
 
   update() {
-    this.position.y += 1;
+    this.draw();
+
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
   }
 }
 
-const player = new Player();
+const player = new Player({ x: 0, y: 0 });
+const player2 = new Player({ x: 200, y: 0 });
 
 function animate() {
   window.requestAnimationFrame(animate);
   context.fillStyle = "white";
   context.fillRect(0, 0, canvas.width, canvas.height);
-  player.draw();
+
   player.update();
+  player2.update();
 }
 animate();
