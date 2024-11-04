b3538a904c5485b40b78eaf1b727ced3c346d2b9
daddyjanno <jn.drugmand@gmail.com>
2024-11-04 12:46:03 +0100

    Player.js with Player class

---
+++ js/classes/Player.js
@@ -0,0 +1,27 @@
+class Player {
+  constructor(position) {
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.height = 100;
+  }
+  draw() {
+    context.fillStyle = "red";
+    context.fillRect(this.position.x, this.position.y, 100, this.height);
+  }
+
+  update() {
+    this.draw();
+
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
