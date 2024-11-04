14f711ad39fcbaa9eaf7c1f86547e9c0308702cd
daddyjanno <jn.drugmand@gmail.com>
2024-11-04 10:36:18 +0100

    Player movement

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ class Player {
     this.draw();
 
     this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+
     if (this.position.y + this.height + this.velocity.y < canvas.height) {
       this.velocity.y += gravity;
     } else {
@@ -36,6 +38,18 @@ class Player {
 const player = new Player({ x: 0, y: 0 });
 const player2 = new Player({ x: 200, y: 0 });
 
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+  up: {
+    pressed: false,
+  },
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
   context.fillStyle = "white";
@@ -43,5 +57,42 @@ function animate() {
 
   player.update();
   player2.update();
+
+  player.velocity.x = 0;
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  }
 }
 animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowRight":
+      keys.right.pressed = true;
+      break;
+    case "ArrowLeft":
+      keys.left.pressed = true;
+      break;
+    case "ArrowUp":
+      player.velocity.y = -15;
+      break;
+
+    default:
+      break;
+  }
+});
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowRight":
+      keys.right.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.left.pressed = false;
+      break;
+
+    default:
+      break;
+  }
+});
