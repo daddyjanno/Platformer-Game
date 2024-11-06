c755f055a8c7a10005d200845e9cda2b935c935c
daddyjanno <jn.drugmand@gmail.com>
2024-11-06 10:46:01 +0100

    create keys.js

---
+++ js/keys.js
@@ -0,0 +1,20 @@
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+  up: {
+    pressed: false,
+  },
+  q: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
