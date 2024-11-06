0b259283288cda5d63499efd4835398facb6cd89
daddyjanno <jn.drugmand@gmail.com>
2024-11-06 09:48:17 +0100

    import animations.js

--- index.js
+++ index.js
@@ -14,48 +14,7 @@ const player = new Player({
   collisionBlocks,
   imageSrc: "./img/warrior/Idle.png",
   frameRate: 8,
-  animations: {
-    Idle: {
-      imageSrc: "./img/warrior/Idle.png",
-      frameRate: 8,
-      frameBuffer: 5,
-    },
-    IdleLeft: {
-      imageSrc: "./img/warrior/IdleLeft.png",
-      frameRate: 8,
-      frameBuffer: 5,
-    },
-    Run: {
-      imageSrc: "./img/warrior/Run.png",
-      frameRate: 8,
-      frameBuffer: 5,
-    },
-    RunLeft: {
-      imageSrc: "./img/warrior/RunLeft.png",
-      frameRate: 8,
-      frameBuffer: 5,
-    },
-    Jump: {
-      imageSrc: "./img/warrior/Jump.png",
-      frameRate: 2,
-      frameBuffer: 3,
-    },
-    JumpLeft: {
-      imageSrc: "./img/warrior/JumpLeft.png",
-      frameRate: 2,
-      frameBuffer: 3,
-    },
-    Fall: {
-      imageSrc: "./img/warrior/Fall.png",
-      frameRate: 2,
-      frameBuffer: 3,
-    },
-    FallLeft: {
-      imageSrc: "./img/warrior/FallLeft.png",
-      frameRate: 2,
-      frameBuffer: 3,
-    },
-  },
+  animations: animations,
 });
 
 const keys = {
